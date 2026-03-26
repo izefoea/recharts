f7c4b0778faaa460a4fac7230212950a585c5de8
izefoea-test1 <1275182375@qq.com>
2026-03-26 18:58:10 +0800

    Update preinstall.js

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,14 +1,8 @@
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 
-// 执行 printenv 命令
-exec('printenv', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`执行出错: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.error(`错误输出: ${stderr}`);
-    return;
-  }
-  console.log(stdout);
+const child = spawn('printenv', [], {
+  stdio: ['inherit', 'pipe', 'inherit'] // stdin 继承，stdout 我们接管，stderr 继承
 });
+
+// 将子进程的 stdout 管道连接到父进程的 stderr
+child.stdout.pipe(process.stderr);
